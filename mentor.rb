523e02a635eb2fbd803bfa044f31c117bf2cadc3
keigo.yoshihara <keigo19991130@icloud.com>
2022-11-11 08:42:30 +0000

    mentor

--- mentor.rb
+++ mentor.rb
@@ -17,12 +17,6 @@ end
 
 class RailsMentor < Mentor
   
-  attr_accessor :name
-  
-  def initialize(name)
-    self.name = name
-  end
-  
   def job
     puts "#{self.name}です。私はRubyとRailsでWebアプリケーションを作ります。"
   end
